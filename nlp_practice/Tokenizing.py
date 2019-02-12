aadd89c9ce9fe689967f561e67d2397fa57a1561
Sahil Gupta <sahil.gupta@smartdatainc.net>
2019-02-12 18:14:36 +0530

    nlp

---
+++ nlp_practice/Tokenizing.py
@@ -0,0 +1,14 @@
+import nltk
+from nltk.tokenize import word_tokenize,sent_tokenize
+from nltk.corpus import stopwords
+
+sentence = """At the eight o'clock on Thursday morning
+... Arthur didn't feel very good."""
+
+tokens = word_tokenize(sentence)
+sw = stopwords.words('english')
+
+print('?????',[t for t in tokens if t not in sw])
+print('done')
+
+
